9e67b24c98e7f217adf53151881ad648b33170c2
Thiago <thiago.souza@e-unicred.com.br>
2021-03-17 15:30:46 -0300

    Criação inicial do projeto

---
+++ src/main/java/com/br/kafka/KafkaPlayingApplication.java
@@ -0,0 +1,13 @@
+package com.br.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPlayingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPlayingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/kafka/KafkaPlayingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlayingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
